c047cbe5fb3483f74e7e8ddf5866f9246ba9c54d
Ethan <ethan@jccsoftware.com>
2023-01-06 01:38:22 +0800

    1.如果沒有config.yaml自動產出 2. 調整lograte參數 3. 調整log level

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/fsnotify/fsnotify"
@@ -17,13 +18,35 @@ var logrotate lumberjack.Logger
 var atomicLevel = zap.NewAtomicLevel()
 
 func init() {
+	content := `logrotate:
+  filename: ./Log/test.log 
+  maxsize: 200 # MB           
+  maxbackups : 10 # 最多保留幾個檔案
+  maxage: 30 # 最多保存天數
+  compress: false # 是否壓縮
+log:
+  level: -1 # -1~4 Debug > Info > Warn > Error > DPanic > Panic`
+
+	if _, err := os.Stat("config.yaml"); err != nil {
+		fmt.Printf("File does not exist\n")
+		var (
+			fileName = "./config.yaml"
+			err2     error
+		)
+		if err2 = ioutil.WriteFile(fileName, []byte(content), 0666); err2 != nil {
+			fmt.Println("Writefile Error =", err2)
+			return
+		}
+	}
+
 	Config = viper.New()
-	Config.SetConfigName("config") // name of config file (without extension)
-	Config.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
-	Config.AddConfigPath(".")      // optionally look for config in the working directory
-	err := Config.ReadInConfig()   // Find and read the config file
-	if err != nil {                // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	Config.SetConfigName("config.yaml") // name of config file (without extension)
+	Config.SetConfigType("yaml")        // REQUIRED if the config file does not have the extension in the name
+	Config.AddConfigPath(".")           // optionally look for config in the working directory
+	err := Config.ReadInConfig()        // Find and read the config file
+	if err != nil {                     // Handle errors reading the config file
+
+		panic(fmt.Errorf("Fatal error config file: %w \nFile content include:\n%s", err, content))
 	}
 	/*
 		err = Config.Unmarshal(&config)
@@ -32,28 +55,19 @@ func init() {
 		}
 	*/
 
-	Config.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("Config file changed:", e.Name)
-		/*
-			atomicLevel.SetLevel(zapcore.ErrorLevel)
-
-			logrotate.MaxSize = 1
-		*/
-		fmt.Println(Config.GetString("test"))
-	})
-	Config.WatchConfig()
-
 	logrotate = lumberjack.Logger{
-		Filename:   "./log", // 日誌文件路徑
-		MaxSize:    10,      // 記錄文件保存的最大尺寸單位：M
-		MaxBackups: 5,       // 日誌文件最多保存多少個備份
-		MaxAge:     28,      // 最多保存多少天的文件文件最多保存多少天
-		Compress:   false,   // 是否壓縮
+		Filename:   Config.GetString("logrotate.filename"), // 日誌文件路徑
+		MaxSize:    Config.GetInt("logrotate.maxsize"),     // 記錄文件保存的最大尺寸單位：M
+		MaxBackups: Config.GetInt("logrotate.maxsize"),     // 日誌文件最多保存多少個備份
+		MaxAge:     Config.GetInt("logrotate.maxsize"),     // 最多保存多少天的文件文件最多保存多少天
+		Compress:   Config.GetBool("logrotate.compress"),   // 是否壓縮
 	}
 
-	//atomicLevel := zap.NewAtomicLevel()
+	atomicLevel := zap.NewAtomicLevel()
 	// 設置日誌級別,可動態修改
-	atomicLevel.SetLevel(zapcore.InfoLevel)
+	changeLogLevel(&atomicLevel)
+
+	//atomicLevel.SetLevel(zapcore.InfoLevel)
 	// public EncoderConfig
 	encoderConfig := zapcore.EncoderConfig{
 		TimeKey:        "time",
@@ -70,16 +84,46 @@ func init() {
 		EncodeName:     zapcore.FullNameEncoder,
 	}
 	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig),                                                // console || json, NewJSONEncoder(),NewConsoleEncoder()
+		zapcore.NewConsoleEncoder(encoderConfig),                                             // console || json, NewJSONEncoder(),NewConsoleEncoder()
 		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&logrotate)), // >> console and file
 		atomicLevel, // defualt log level
 	)
 
-	Log = zap.New(core, zap.AddCaller()) //zap.AddCaller() ,zap.AddStacktrace(),zap.Fields(zap.String("appName", name)),...
+	Log = zap.New(core) //zap.AddCaller() ,zap.AddStacktrace(),zap.Fields(zap.String("appName", name)),...
 	defer Log.Sync()
+
+	Config.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+		changeLogLevel(&atomicLevel)
+		Test()
+	})
+	Config.WatchConfig()
 
 }
 
 func Test() {
-	fmt.Printf("tttttttttttttt")
+	Log.Info("Info")
+	Log.Debug("Debug")
+	Log.Warn("Warn")
+	Log.Error("Error")
+}
+
+func changeLogLevel(atomicLevel *zap.AtomicLevel) {
+	switch x := Config.GetInt("log.level"); x {
+	case -1:
+		atomicLevel.SetLevel(zapcore.DebugLevel)
+	case 0:
+		atomicLevel.SetLevel(zapcore.InfoLevel)
+	case 1:
+		atomicLevel.SetLevel(zapcore.WarnLevel)
+	case 2:
+		atomicLevel.SetLevel(zapcore.ErrorLevel)
+	case 3:
+		atomicLevel.SetLevel(zapcore.DPanicLevel)
+	case 4:
+		atomicLevel.SetLevel(zapcore.PanicLevel)
+	default:
+		atomicLevel.SetLevel(zapcore.DebugLevel)
+
+	}
 }
